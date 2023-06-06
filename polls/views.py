e8204250ffeaec827d62ebc7042ef200f3eaf0cb
mitcy saraiva <saraivamitcy@gmail.com>
2023-06-06 11:05:58 -0300

    exibe as questions

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
+from polls.models import Question
 
 # View index ... carregada para alguma rota (caminho)
 def index(request):
@@ -10,3 +10,7 @@ def index(request):
 
 def sobre(request):
     return HttpResponse('Este é um app de enquete!')
+
+def exibe_questao(request, question_id):
+    questao = Question.objects.get(id=question_id)
+    return HttpResponse(questao.question_text)
